8debacc0a46e6c8d08e1fa11136354b55e054a7d
Erik Galler <43548211+erikgal@users.noreply.github.com>
2022-02-28 18:50:47 +0100

    Set up penalty fitness

--- src/EA.java
+++ src/EA.java
@@ -8,9 +8,9 @@ import org.json.simple.parser.ParseException;
 public class EA {
     public static void main(String args[]) {
         // Hyper-parameters
-        int epochs = 100;
+        int epochs = 20;
         int trainInstanceIndex = 2;
-        int popSize = 5000;
+        int popSize = 1000;
         double pC = 0.9;
         double pM = 0.007;
         int lambda = 7; // 3 is more populare now a days
@@ -33,29 +33,32 @@ public class EA {
         ArrayList<Individual> population = populationClass.generatePopArray(trainInstance, nbrNurses, popSize);
 
         for (int epoch = 1; epoch < epochs; epoch++) {
-
+            // Fitness of new Popuation
             Fitness fitnessClass = new Fitness(nbrNurses, capacityNurse, depot, patients, travelTimes);
-            ArrayList<Double> populationgFitness = fitnessClass.getRegularFitness(population);
+            ArrayList<Double> populationgFitness = fitnessClass.getPenaltyFitness(population);
             double popMaxFitVal = fitnessClass.prevMaxFitness;
-            System.out.println(fitnessClass.prevMinFitness);
+            System.out.println("Min Fitness " + fitnessClass.prevMinFitness);
+            System.out.println("Min Penalty " + fitnessClass.prevMinPenalty);
             ArrayList<Double> transFitness = fitnessClass.transformFitnessArray(populationgFitness, popMaxFitVal);
 
+            // Parent Selection
             // Consider to normalize fitness for greater selection pressure
             Parent parentClass = new Parent(nbrNurses, capacityNurse, depot, patients, travelTimes);
             ArrayList<Individual> parents = parentClass.selectParentsProbabilistic(transFitness, population,
                     popSize);
             ArrayList<Double> parentTransFitness = parentClass.parentFitness;
 
+            // Offspring
             Offspring offspringClass = new Offspring(nbrNurses, capacityNurse, depot, patients, travelTimes);
             ArrayList<Individual> offspring = offspringClass.createOffspring(parents, parentTransFitness, pC,
                     pM, lambda);
-            ArrayList<Double> offspringFitness = fitnessClass.getRegularFitness(offspring); // This can definitly
+            ArrayList<Double> offspringFitness = fitnessClass.getPenaltyFitness(offspring); // This can definitly
                                                                                                  // be optimized
             double offMaxFitVal = fitnessClass.prevMaxFitness;
             ArrayList<Double> offspringTransFitness = fitnessClass.transformFitnessArray(offspringFitness,
                     offMaxFitVal);
 
-
+            // Survivor Selection
             // (lambda, mu)-selection, based on offspring only (lambda > mu)
             Survivor survivorClass = new Survivor(nbrNurses, capacityNurse, depot, patients, travelTimes);
             ArrayList<Individual> survivors = survivorClass.deterministicOffspringSelection(offspring,
--- src/Fitness.java
+++ src/Fitness.java
@@ -9,6 +9,7 @@ public class Fitness {
     public Patient[] patients;
     public double prevMaxFitness;
     public double prevMinFitness;
+    public double prevMinPenalty;
     public ArrayList<ArrayList<Double>> fitnessMatrix; // Keeps record of the fitness for each path
 
     public Fitness(int nbrNurses, int capacityNurse, Depot depot, Patient[] patients, double[][] travelTimes) {
@@ -19,6 +20,7 @@ public class Fitness {
         this.patients = patients;
     }
 
+    // Naively checks route duration without any feasibility check
     public ArrayList<Double> getRegularFitness(ArrayList<Individual> population) {
         ArrayList<Double> fitness = new ArrayList<Double>();
         double maxVal = 0;
@@ -39,6 +41,85 @@ public class Fitness {
             }
             fitness.add(indFitness);
         }
+        this.prevMaxFitness = maxVal;
+        this.prevMinFitness = minVal;
+
+        return fitness;
+    }
+
+    public ArrayList<Double> getPenaltyFitness(ArrayList<Individual> population) {
+        ArrayList<Double> fitness = new ArrayList<Double>();
+        double maxVal = 0;
+        double minVal = Math.pow(10, 10);
+        double minPenVal = Math.pow(10, 10);
+        double penaltyScale = 100;
+        for (int i = 0; i < population.size(); i++) {
+
+            Individual individual = population.get(i);
+            ArrayList<Integer> routes = individual.routes;
+
+            double travelDuration = 0;  
+            double penaltyCapacity = 0;
+            double penaltyMissedCareTime = 0; 
+
+            double nurseClock = 0;            
+            double nurseUsage = 0;
+
+            for (int j = 0; j < routes.size() - 1; j++) {
+                Patient patient = this.patients[routes.get(j + 1)];
+                double travel = travelTimes[routes.get(j)][routes.get(j + 1)];
+                nurseClock += travel;
+                travelDuration += travel;
+
+                if(nurseClock < patient.start_time){ // Wait
+                    nurseClock = patient.start_time;
+                }
+
+                if(nurseClock > patient.end_time){ // Missed patient entirely
+                    penaltyMissedCareTime = patient.care_time + (nurseClock - patient.end_time);
+                } 
+
+                else { // Arrived in time window
+                    double availableCareTime =  patient.end_time - nurseClock;
+                    double restCareTime = availableCareTime - patient.care_time;
+                    if(restCareTime < 0){ // Penalty! Arrived too late
+                        penaltyMissedCareTime += -restCareTime; // Care time missing
+                        nurseClock = patient.end_time;
+                        nurseUsage += availableCareTime;
+                    }
+                    else{ // Nailed it!
+                        nurseUsage += patient.care_time;
+                        nurseClock += patient.care_time;
+                    }
+                }
+
+                if (routes.get(j + 1) == 0){ // Arrival at Depot
+                    if(nurseUsage > capacityNurse){ // To much work for one nurse
+                        penaltyCapacity += nurseUsage - capacityNurse;
+                    }
+                    nurseUsage = 0; // Reset for new nurse
+                    nurseClock = 0;
+                }
+            }
+            double penalty = (penaltyMissedCareTime + penaltyCapacity) * penaltyScale;
+            double penaltyFitness = travelDuration + penalty;
+            if( (penaltyMissedCareTime + penaltyCapacity) == 0){
+                System.out.println("LGTM!");
+            }
+
+            fitness.add(penaltyFitness);
+
+            if(penaltyFitness > maxVal){
+                maxVal = penaltyFitness;
+            }
+            if(penaltyFitness < minVal){
+                minVal = penaltyFitness;
+            }
+            if(penalty < minPenVal){
+                this.prevMinPenalty = penalty;
+            }
+        }
+
         this.prevMaxFitness = maxVal;
         this.prevMinFitness = minVal;
 
